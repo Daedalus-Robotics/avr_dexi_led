26b2af9df85d70c2429a2da8a1fc5d5f962f7f0d
owaisoumera <139817035+owaisoumera@users.noreply.github.com>
2024-11-23 16:14:46 -0500

    Create alternate-animation.py

---
+++ avr_dexi_led/alternate-animation.py
@@ -0,0 +1,32 @@
+from adafruit_led_animation.animation import Animation
+
+class Alternate(Animation):
+
+    def __init__(self, pixel_object, speed, color_a, color_b):
+        super().__init__(pixel_object, speed, color_a, color_b)
+
+        self.color_a = color_a
+        self.color_b = color_b
+        self._tick = 0
+        self._tick_end = 4
+
+    def draw(self):
+        if self.tick%2 == 0:
+            try:
+                # Selects even item and sets it to Color A
+                self.pixel_object[::2] = [self.color_a]*(len(self.pixel_object)//2)
+                # Selects odd item and sets it to Color B
+                self.pixel_object[1::2] = [self.color_b]*(len(self.pixel_object)//2)
+            except IndexError:
+                pass
+        else:
+            self.pixel_object.fill((0,0,0))
+
+        if self._tick > self._tick_end:
+            # call internal reset() function
+            self.reset()
+
+        self._tick+=1
+    def reset(self):
+        self.pixel_object.fill((0,0,0))
+        self._tick = 0
